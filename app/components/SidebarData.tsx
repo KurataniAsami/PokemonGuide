b388e453d6b6dc0adeb8ef7f793fbc91cacc3b29
kurataniasami <kr-scream.hurryx@ezweb.ne.jp>
2026-05-28 18:55:30 +0900

    0528

---
+++ app/components/SidebarData.tsx
@@ -0,0 +1,19 @@
+import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
+
+export const SidebarData = [
+  {
+    title: "ポケモン一覧",
+    icon: <LaptopChromebookIcon/>,
+    link: "/home"
+  },
+  {
+    title: "タイプ",
+    icon: <LaptopChromebookIcon/>,
+    link: "/type"
+  },
+  {
+    title: "進化",
+    icon: <LaptopChromebookIcon/>,
+    link: "/evolution"
+  },
+]
